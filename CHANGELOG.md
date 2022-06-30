3b4c7daec0874381824bd74efc33ecf8e2864e0f
mehmet <mehmedyaz@gmail.com>
2022-07-01 02:26:00 +0300

    export sign package

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.0.1+1
+
+* Export sign package
+
 ## 0.0.1
 
 * TODO: Describe initial release.
--- lib/sign_flutter.dart
+++ lib/sign_flutter.dart
@@ -3,3 +3,4 @@ export 'src/consumer_widget.dart';
 export 'src/global_consumer.dart';
 export 'src/global_slot_widget.dart';
 export 'src/slot_widget.dart';
+export 'package:sign/sign.dart';
