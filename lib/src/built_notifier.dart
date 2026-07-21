9684f1867a3958b9a06fbb27b4ab38a1106109f1
Mehmetyaz <mehmedyaz@gmail.com>
2026-07-22 00:26:26 +0300

    refactor: update BuiltNotifier visual feedback to use foreground decoration and reactive parameter updates

--- lib/src/built_notifier.dart
+++ lib/src/built_notifier.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 
-/// Built Notifier paint your widget after built
+/// Built Notifier paints a temporary color overlay over your widget whenever it rebuilds.
 class BuiltNotifier extends StatefulWidget {
   /// It reaches from specified color to
   /// transparent color in the specified time.
   const BuiltNotifier({
     super.key,
     required this.child,
-    this.duration = const Duration(milliseconds: 250),
-    this.color = Colors.red,
+    this.duration = const Duration(milliseconds: 350),
+    this.color = const Color(0x66FF0000),
   });
 
   /// Your widget
@@ -31,16 +31,21 @@ class _BuiltNotifierState extends State<BuiltNotifier>
 
   @override
   void initState() {
+    super.initState();
     _controller = AnimationController(vsync: this, duration: widget.duration);
     _animation = _controller.drive(
       ColorTween(begin: widget.color, end: Colors.transparent),
     );
-    super.initState();
   }
 
-  Future<void> _start() async {
-    _controller.reset();
-    await _controller.forward();
+  @override
+  void didUpdateWidget(covariant BuiltNotifier oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.color != widget.color || oldWidget.duration != widget.duration) {
+      _animation = _controller.drive(
+        ColorTween(begin: widget.color, end: Colors.transparent),
+      );
+    }
   }
 
   @override
@@ -49,13 +54,36 @@ class _BuiltNotifierState extends State<BuiltNotifier>
     super.dispose();
   }
 
+  void _triggerFlash() {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      if (mounted) {
+        _controller.forward(from: 0.0);
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    _start();
+    _triggerFlash();
     return AnimatedBuilder(
       animation: _animation,
-      builder: (c, b) =>
-          Container(color: _animation.value ?? Colors.white, child: b),
+      builder: (context, child) {
+        final currentColor = _animation.value;
+        if (currentColor == null || currentColor.a == 0) {
+          return child!;
+        }
+        return DecoratedBox(
+          position: DecorationPosition.foreground,
+          decoration: BoxDecoration(
+            color: currentColor,
+            border: Border.all(
+              color: currentColor.withAlpha(255),
+              width: 1.5,
+            ),
+          ),
+          child: child,
+        );
+      },
       child: widget.child,
     );
   }
